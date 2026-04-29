40dda96db9f629eb66208f8e4abd106471d5a10e
Ryoroly <alexandrugabrielserban@gmail.com>
2026-04-29 19:57:00 +0300

    Primesc datele de pe telefon si pot sa le afisez pe serial monitor chiar daca sunt despartite ca si cum ar fi acceleratiile pe cele 3 axe

--- main.ino
+++ main.ino
@@ -9,7 +9,62 @@
 // clasa pentru a prelua datele care vin de la telefon
 class MyCallbacks: public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharacteristic) {
+      String rxValue = pCharacteristic->getValue(); #include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+// la fel, pastram UUID-urile
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+// aici procesam ce vine de la telefon
+class MyCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
       String rxValue = pCharacteristic->getValue(); 
+
+      if (rxValue.length() > 0) {
+        Serial.print("Giroscop [X, Y, Z]: ");
+        
+        // afisam fix valorile primite
+        for (int i = 0; i < rxValue.length(); i++) {
+          Serial.print(rxValue[i]);
+        }
+        Serial.println();
+        
+        // aici pe viitor vei pune formula ta de calcul 
+        // ca sa vezi daca soferul e agresiv
+      }
+    }
+};
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Pornire sistem telemetrie...");
+
+  BLEDevice::init("ESP32_Giroscop"); 
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID,
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+
+  pCharacteristic->setCallbacks(new MyCallbacks());
+  pService->start();
+
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  BLEDevice::startAdvertising();
+  
+  Serial.println("BLE pornit. Astept date din aplicatie...");
+}
+
+void loop() {
+  // procesorul e liber, toata treaba se face in onWrite cand vin date
+  delay(2000); 
+}
 
       if (rxValue.length() > 0) {
         Serial.print("Date noi primite: ");
