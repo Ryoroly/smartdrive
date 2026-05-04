c583c94eb0ecbf6a7baf66ec4d6ea9fb22756c9b
Ryoroly <alexandrugabrielserban@gmail.com>
2026-05-04 21:30:02 +0300

    Esp primeste date de la telefon plrin BLE si le paote afisa in timp real , se verifica daca esp a primti datele sau nu

--- main.ino
+++ main.ino
@@ -6,74 +6,43 @@
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
-// aici procesam ce vine de la telefon
-// class MyCallbacks: public BLECharacteristicCallbacks {
-//     void onWrite(BLECharacteristic *pCharacteristic) {
-//       String rxValue = pCharacteristic->getValue(); 
 
-//       if (rxValue.length() > 0) {
-//         Serial.print("Giroscop [X, Y, Z]: ");
-        
-//         // afisam fix valorile primite
-//         for (int i = 0; i < rxValue.length(); i++) {
-//           Serial.print(rxValue[i]);
-//         }
-//         Serial.println();
-        
-//         // aici pe viitor vei pune formula ta de calcul 
-//         // ca sa vezi daca soferul e agresiv
-//       }
-//     }
-// };
-
-// aici procesam ce vine de la telefon
 class MyCallbacks: public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharacteristic) {
-      // Preluam valoarea bruta ca std::string
-      std::string rxValue = pCharacteristic->getValue(); 
+      
+      String data = pCharacteristic->getValue(); 
 
-      if (rxValue.length() > 0) {
-        // Convertim in String de Arduino pentru a folosi functii mai usoare
-        String data = String(rxValue.c_str());
+      // 1. AFISAM DATELE BRUTE EXACT CUM VIN (pentru testare)
+      Serial.print("Date primite de la Flutter: ");
+      Serial.println(data);
+
+      if (data.length() > 0) {
         
-        // 1. Căutăm unde sunt virgulele în textul primit
         int firstComma = data.indexOf(',');
         int secondComma = data.indexOf(',', firstComma + 1);
 
-        // Dacă am găsit ambele virgule, înseamnă că formatul este corect
         if (firstComma != -1 && secondComma != -1) {
           
-          // 2. Extragem bucățile de text dintre virgule și le facem numere (float)
           float gyroX = data.substring(0, firstComma).toFloat();
           float gyroY = data.substring(firstComma + 1, secondComma).toFloat();
           float gyroZ = data.substring(secondComma + 1).toFloat();
 
-          // 3. Afișăm valorile ordonat în Serial Monitor
-          Serial.print("Valori Giroscop -> X: "); Serial.print(gyroX);
+          Serial.print("Valori Decodate -> X: "); Serial.print(gyroX);
           Serial.print(" | Y: "); Serial.print(gyroY);
           Serial.print(" | Z: "); Serial.println(gyroZ);
 
-          // ---------------------------------------------------------
-          // 4. LOGICA TA PENTRU TELEMETRIE / CONDUS AGRESIV
-          // ---------------------------------------------------------
-          // Presupunând că telefonul stă vertical în suport, 
-          // axa Y sau Z va înregistra rotația stânga/dreapta.
-          // Valorile sunt în radiani pe secundă (rad/s).
-          
-          float pragViraj = 1.2; // Sensibilitatea. Modifică acest număr după teste.
+          float pragViraj = 1.2; 
 
-          // Verificăm axa Z (sau Y, depinde cum ții telefonul în mașină)
           if (gyroZ > pragViraj) {
             Serial.println("ALERTA: Viraj strâns la STÂNGA!");
-            // Aici poti aprinde un LED, declansa un buzzer etc.
-            // digitalWrite(LED_PIN, HIGH);
           } 
           else if (gyroZ < -pragViraj) {
             Serial.println("ALERTA: Viraj strâns la DREAPTA!");
-            // digitalWrite(LED_PIN, HIGH);
           }
           
           Serial.println("-----------------------------------");
+        } else {
+          Serial.println("Eroare: Format gresit (lipsesc virgulele)!");
         }
       }
     }
@@ -87,9 +56,11 @@ void setup() {
   BLEServer *pServer = BLEDevice::createServer();
   BLEService *pService = pServer->createService(SERVICE_UUID);
 
+  // AICI AM REPARAT CONFLICTUL: Am permis atat WRITE cat si WRITE_NR
   BLECharacteristic *pCharacteristic = pService->createCharacteristic(
                                          CHARACTERISTIC_UUID,
-                                         BLECharacteristic::PROPERTY_WRITE
+                                         BLECharacteristic::PROPERTY_WRITE | 
+                                         BLECharacteristic::PROPERTY_WRITE_NR
                                        );
 
   pCharacteristic->setCallbacks(new MyCallbacks());
